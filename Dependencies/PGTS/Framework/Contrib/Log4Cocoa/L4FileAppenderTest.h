17fc1aad419bea8a68cb4efe80a426ee62e87aa5
tuukka.norri@karppinen.fi <tuukka.norri@karppinen.fi@fe1aa653-7022-0410-af67-bafd91c7e665>
2007-01-03 17:06:14 +0000

    Added the Log4Cocoa logging framework
    git-svn-id: http://basetenframework.org/svn/trunk@69 fe1aa653-7022-0410-af67-bafd91c7e665

---
+++ Dependencies/PGTS/Framework/Contrib/Log4Cocoa/L4FileAppenderTest.h
@@ -0,0 +1,19 @@
+//
+//  L4FileAppenderTest.h
+//  Log4Cocoa
+//
+//  Created by Michael James on Tue Apr 27 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <ObjcUnit/ObjcUnit.h>
+
+@class L4FileAppender;
+
+@interface L4FileAppenderTest : TestCase
+{
+	L4FileAppender*		_fileAppender;
+}
+
+@end
---
+++ Dependencies/PGTS/Framework/Contrib/Log4Cocoa/L4PatternLayoutTest.h
@@ -0,0 +1,21 @@
+//
+//  L4PatternLayoutTest.h
+//  Log4Cocoa
+//
+//  Created by Michael James on Wed Mar 03 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class L4PatternLayout;
+
+@interface L4PatternLayoutTest : TestCase
+{
+	L4PatternLayout*	_pLayout;
+}
+
+- (void)setUp;
+- (void)tearDown;
+
+@end
