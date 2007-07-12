f84e9e5639de49b378efacb5a6994d68f8f7990c
tuukka.norri@karppinen.fi <tuukka.norri@karppinen.fi@fe1aa653-7022-0410-af67-bafd91c7e665>
2007-07-12 08:33:55 +0000

    Groundwork for new relationship classes
    git-svn-id: http://basetenframework.org/svn/trunk@225 fe1aa653-7022-0410-af67-bafd91c7e665

---
+++ BXRelatedObjectContainer.m
@@ -0,0 +1,54 @@
+//
+// BXRelatedObjectContainer.m
+// BaseTen
+//
+// Copyright (C) 2007 Marko Karppinen & Co. LLC.
+//
+// Before using this software, please review the available licensing options
+// by visiting http://www.karppinen.fi/baseten/licensing/ or by contacting
+// us at sales@karppinen.fi. Without an additional license, this software
+// may be distributed only in compliance with the GNU General Public License.
+//
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License, version 2.0,
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// $Id$
+//
+
+#import "BXRelatedObjectContainer.h"
+
+
+@implementation BXRelatedObjectContainer
+
+- (void) dealloc
+{
+    [mObject release];
+    [super dealloc];
+}
+
+- (void) setObject: (id) anObject
+{
+    if (anObject != mObject)
+    {
+        [mObject release];
+        mObject = [anObject retain];
+    }
+}
+
+- (id) object
+{
+    return [[mObject retain] autorelease];
+}
+
+@end
