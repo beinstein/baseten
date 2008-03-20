ce03b85710919b340296f193fe1013c09179bc17
tuukka.norri@karppinen.fi <tuukka.norri@karppinen.fi@fe1aa653-7022-0410-af67-bafd91c7e665>
2008-03-20 14:17:21 +0000

    Fixed a gc-related problem which caused crashes - Used CFRetain and CFRelease. A more thorough fix will be needed.
    git-svn-id: http://basetenframework.org/svn/trunk@423 fe1aa653-7022-0410-af67-bafd91c7e665

--- Dependencies/PGTS/Framework/Sources/TSRunloopMessenger.m
+++ Dependencies/PGTS/Framework/Sources/TSRunloopMessenger.m
@@ -28,6 +28,17 @@
 
 #import "TSRunloopMessenger.h"
 
+/*
+ * struct message
+ */
+struct message
+{
+	NSConditionLock		* resultLock;
+	NSInvocation			* invocation;
+};
+
+extern void sendData( NSData * aData, NSPort * aPort );
+
 
 @implementation TSRunloopMessenger
 
@@ -42,6 +53,59 @@
 - (id)target:(id)aTarget withResult:(BOOL)aResultFlag;
 {
 	return [[[TSRunloopMessengerForwardingProxy alloc] _initWithTarget:aTarget withOwner:self withResult:aResultFlag] autorelease];
+}
+
+- (void)messageInvocation:(NSInvocation *)anInvocation withResult:(BOOL)aResultFlag
+{
+	struct message		* theMessage;
+	NSMutableData		* theData;
+	NSConditionLock		* theLock;
+	
+	[anInvocation retainArguments];
+	
+	theData = [NSMutableData dataWithLength:sizeof(struct message)];
+	theMessage = (struct message *)[theData mutableBytes];
+	
+	theMessage->invocation = (id) CFRetain (anInvocation);		// will be released by handlePortMessage
+	if (aResultFlag)
+	{
+		theLock = [[NSConditionLock alloc] initWithCondition: NO];
+		theMessage->resultLock = theLock;
+	}
+	else
+	{
+		theMessage->resultLock = nil;
+	}
+	
+	sendData( theData, port );
+	
+	if( aResultFlag )
+	{
+		[theMessage->resultLock lockWhenCondition:YES];
+		[theMessage->resultLock unlock];
+		[theMessage->resultLock release];
+	}
+}
+
+- (void)handlePortMessage:(NSPortMessage *)aPortMessage
+{
+	struct message 	* theMessage;
+	NSData				* theData;
+	void					handlePerformSelectorMessage( struct message * aMessage );
+	void					handleInvocationMessage( struct message * aMessage );
+	
+	theData = [[aPortMessage components] lastObject];
+	
+	theMessage = (struct message *)[theData bytes];
+	
+	[theMessage->invocation invoke];
+	if( theMessage->resultLock )
+	{
+		[theMessage->resultLock lock];
+		[theMessage->resultLock unlockWithCondition:YES];
+	}
+	
+	CFRelease (theMessage->invocation);	// to balance messageInvocation:withResult:
 }
 
 @end
