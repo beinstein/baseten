7bf790ff6aef2fafe53d47185013d7bf31c2e91a
tsnorri <tsnorri@fe1aa653-7022-0410-af67-bafd91c7e665>
2009-04-29 15:18:26 +0000

    Added another model test
    git-svn-id: http://basetenframework.org/svn/trunk@1128 fe1aa653-7022-0410-af67-bafd91c7e665

--- UnitTests/Sources/BXDataModelTests.m
+++ UnitTests/Sources/BXDataModelTests.m
@@ -56,15 +56,16 @@
 {
 }
 
-- (void) testPeopleDepartments
+
+- (NSArray *) importStatements: (NSString *) modelFile
 {
 	NSBundle* bundle = [NSBundle bundleForClass: [self class]];
-	NSString* path = [bundle pathForResource: @"people-departments" ofType: @"mom"];
+	NSString* path = [bundle pathForResource: modelFile ofType: @"mom"];
 	NSURL* url = [NSURL fileURLWithPath: path];
 	MKCAssertNotNil (bundle);
 	MKCAssertNotNil (path);
 	MKCAssertNotNil (url);
-		
+	
 	NSManagedObjectModel* model = [[[NSManagedObjectModel alloc] initWithContentsOfURL: url] autorelease];
 	NSArray* entities = [model entities];
 	MKCAssertNotNil (model);
@@ -73,7 +74,14 @@
 	[mImporter setSchemaName: @"test_schema"];
 	[mImporter setEntities: entities];
 	NSArray* statements = [mImporter importStatements];
+	MKCAssertNotNil (statements);
 	
+	return statements;
+}
+
+- (void) testPeopleDepartments
+{
+	NSArray* statements = [self importStatements: @"people-departments"];
 	NSArray* expected = [NSArray arrayWithObjects:
 						 @"CREATE SCHEMA \"test_schema\";",
 						 @"CREATE TABLE \"test_schema\".\"Person\" (id SERIAL, \"surname\" text , \"birthday\" timestamp with time zone ) ;",
@@ -84,6 +92,30 @@
 						 @"ALTER TABLE \"test_schema\".\"Department\" ADD PRIMARY KEY (id);",
 						 @"ALTER TABLE \"test_schema\".\"Employee\" ADD COLUMN \"department_id\" integer;",
 						 @"ALTER TABLE \"test_schema\".\"Employee\" ADD CONSTRAINT \"department__employee\"   FOREIGN KEY (\"department_id\") REFERENCES \"test_schema\".\"Department\" (id)   ON DELETE SET NULL ON UPDATE CASCADE;",
+						 nil];
+	MKCAssertEqualObjects (expected, statements);
+}
+
+- (void) testOneToOne
+{
+	NSArray* statements = [self importStatements: @"one-to-one"];
+	NSArray* expected = [NSArray arrayWithObjects:
+						 @"CREATE SCHEMA \"test_schema\";",
+						 @"CREATE TABLE \"test_schema\".\"a\" (id SERIAL) ;",
+						 @"ALTER TABLE \"test_schema\".\"a\" ADD PRIMARY KEY (id);",
+						 @"CREATE TABLE \"test_schema\".\"b\" (id SERIAL) ;",
+						 @"ALTER TABLE \"test_schema\".\"b\" ADD PRIMARY KEY (id);",
+						 @"CREATE TABLE \"test_schema\".\"c\" (id SERIAL) ;",
+						 @"ALTER TABLE \"test_schema\".\"c\" ADD PRIMARY KEY (id);",
+						 @"CREATE TABLE \"test_schema\".\"d\" (id SERIAL) ;",
+						 @"ALTER TABLE \"test_schema\".\"d\" ADD PRIMARY KEY (id);",
+						 @"ALTER TABLE \"test_schema\".\"a\" ADD COLUMN \"ab_id\" integer;",
+						 @"ALTER TABLE \"test_schema\".\"a\" ALTER COLUMN \"ab_id\" SET NOT NULL;",
+						 @"ALTER TABLE \"test_schema\".\"a\" ADD UNIQUE (\"ab_id\");",
+						 @"ALTER TABLE \"test_schema\".\"a\" ADD CONSTRAINT \"ab__ba\"   FOREIGN KEY (\"ab_id\") REFERENCES \"test_schema\".\"b\" (id)   ON DELETE SET NULL ON UPDATE CASCADE;",
+						 @"ALTER TABLE \"test_schema\".\"c\" ADD COLUMN \"cd_id\" integer;",
+						 @"ALTER TABLE \"test_schema\".\"c\" ADD UNIQUE (\"cd_id\");",
+						 @"ALTER TABLE \"test_schema\".\"c\" ADD CONSTRAINT \"cd__dc\"   FOREIGN KEY (\"cd_id\") REFERENCES \"test_schema\".\"d\" (id)   ON DELETE SET NULL ON UPDATE CASCADE;",
 						 nil];
 	MKCAssertEqualObjects (expected, statements);
 }
