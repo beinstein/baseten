41389b1ae45d99166af522cca0cf7f526e1d5921
tsnorri <tsnorri@fe1aa653-7022-0410-af67-bafd91c7e665>
2009-06-10 00:12:04 +0000

    Added a bare-bones example project - The project fetches rows from a table in basetentest.
    git-svn-id: http://basetenframework.org/svn/trunk@1303 fe1aa653-7022-0410-af67-bafd91c7e665

---
+++ Examples/BasicFunctionality/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BasicFunctionality
+//
+//  Created by Tuukka Norri on 30.3.2009.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
