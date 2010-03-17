337c16fce642adbca40127508b6349a6981679df
tsnorri <tsnorri@fe1aa653-7022-0410-af67-bafd91c7e665>
2010-03-17 17:48:35 +0000

    Fixed typo in copy-items-for-assistant.sh
    git-svn-id: http://basetenframework.org/svn/trunk@1406 fe1aa653-7022-0410-af67-bafd91c7e665

--- Tools/copy-items-for-assistant.sh
+++ Tools/copy-items-for-assistant.sh
@@ -6,7 +6,7 @@ then
     ib_plugin="$SRCROOT/../InterfaceBuilderPlugin/build/$BUILD_STYLE/BaseTen.ibplugin"
     if [ -d "$ib_plugin" ]
     then
-        "$CP" -a -f -v "$ib_pluin" "$BUILT_PRODUCTS_DIR"
+        "$CP" -a -f -v "$ib_plugin" "$BUILT_PRODUCTS_DIR"
     else
         echo "Didn't find BaseTen.ibplugin!"
         exit 1
